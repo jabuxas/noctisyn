21bc809af0a887286ce45319be653b197e8c0fd5
lucas barbieri <lucas@jabuxas.com>
2025-12-11 16:12:21 -0300

    feat: able to search for a novel specifically

--- cmd/noctisyn/main.go
+++ cmd/noctisyn/main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-
-	"github.com/gocolly/colly/v2"
-)
+import "github.com/jabuxas/noctisyn/internal/ln"
 
 func main() {
-	c := colly.NewCollector()
-
-	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
-		link := h.Attr("href")
-		chapterHref := strings.Contains(link, "chapter")
-		if !chapterHref {
-			h.Request.Visit(link)
-		}
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("visiting", r.URL)
-	})
-
-	c.Visit("https://novelfull.net")
+	ln.Scrape("warlock magus")
 }
---
+++ internal/ln/utils.go
@@ -0,0 +1,20 @@
+package ln
+
+import (
+	"strings"
+)
+
+func wordsInOrder(s, query string) bool {
+	sLower := strings.ToLower(s)
+	words := strings.Fields(strings.ToLower(query))
+
+	pos := 0
+	for _, w := range words {
+		idx := strings.Index(sLower[pos:], w)
+		if idx == -1 {
+			return false
+		}
+		pos += idx + len(w)
+	}
+	return true
+}
